39ff10887c773b5af5f12b01169de71271e8a42d
rlena <lena.ricard@ynov.com>
2025-03-14 15:11:49 +0100

    Quality part equals

--- src/fr/ynov/java/medium/Quality.java
+++ src/fr/ynov/java/medium/Quality.java
@@ -1,5 +1,7 @@
 package fr.ynov.java.medium;
 
+import java.util.Objects;
+
 public class Quality {
 
     public Integer id;
@@ -10,10 +12,22 @@ public class Quality {
         this.name = name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Quality quality = (Quality) o;
+        return Objects.equals(id, quality.id) && Objects.equals(name, quality.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
     public static void main(String[] args) {
         Quality q = new Quality(1, "Yes");
         Quality q2 = new Quality(1, "Yes");
 
-        System.out.println(q == q2);
+        System.out.println(q.equals(q2));
     }
 }
