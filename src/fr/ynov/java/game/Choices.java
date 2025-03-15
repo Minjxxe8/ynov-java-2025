746a7da9c7036526980c227a5471cc9d5c974816
rlena <lena.ricard@ynov.com>
2025-03-15 02:15:07 +0100

    Player Choice not finished, without error handler

--- src/fr/ynov/java/game/Choices.java
+++ src/fr/ynov/java/game/Choices.java
@@ -1,5 +1,31 @@
 package fr.ynov.java.game;
 
+import java.util.Scanner;
+
 public class Choices {
 
+   String choice;
+
+   public Choices() {
+       this.choice = Playerchoice();
+   }
+
+   public String Playerchoice() {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter a number : ");
+       int number = sc.nextInt();
+
+       String choice = switch (number) {
+           case 1 -> "rock";
+           case 2 -> "paper";
+           case 3 -> "scissors";
+           default -> "";
+       };
+
+       if (number < 1 || number > 3) {
+           System.out.println("Invalid Choice, please choose a number between 1 and 3");
+           return Playerchoice();
+       }
+       return choice;
+   }
 }
--- src/fr/ynov/java/game/Game.java
+++ src/fr/ynov/java/game/Game.java
@@ -1,7 +1,10 @@
 package fr.ynov.java.game;
 
+import java.awt.*;
+
 public class Game {
     public static void main(String[] args) {
         Player player = new Player();
+        Choices choice = new Choices();
     }
 }
